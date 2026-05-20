1a3954762a5bb24909ac9bf78c3f5de10f9a6768
MaisaMarcal <maisa_marcal@estudante.sesisenai.org.br>
2026-05-19 21:32:20 -0300

    Criando

---
+++ listartarefas/src/main/java/com/tecdes/listartarefas/ListartarefasApplication.java
@@ -0,0 +1,13 @@
+package com.tecdes.listartarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListartarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListartarefasApplication.class, args);
+	}
+
+}
---
+++ listartarefas/src/test/java/com/tecdes/listartarefas/ListartarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecdes.listartarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListartarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
